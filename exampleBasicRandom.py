9a1769ad037ea18c4437200968db0a27cd800461
Daniel <djisenberg2015@gmail.com>
2020-06-20 14:39:05 -0400

    Made addititional formatting improvements and fixed a few minor aesthetic issues like the y axis label not being displayed, the legend not being displayed, etc. Also merged the double line print statements into a single print statement using the \n notation. Removed unused/deprecated code.

--- exampleBasicRandom.py
+++ exampleBasicRandom.py
@@ -21,42 +21,25 @@ import matplotlib.pyplot as plt
 maxPoints = 1000
 
 # Doing this one step at a time would be slow, but possible
-print("Scalar method of generating random number sequence")
-print("Uniform distribution")
-
+print("Scalar method of generating random number sequence\nUniform distribution")
 iList = []
 xList = []
 for i in range(maxPoints):
     x = np.random.random_sample()
     iList.append(i)
     xList.append(x)
-    #print (i, x)
+    # print(i, x)
 
 plt.plot(iList, xList, color="red", label="list", linestyle="None", marker=".")
 
 # generate sequence that is maxPoints long and store in an array
-print("Array method of generating random number sequence")
-print("Uniform distribution")
-
+print("Array method of generating random number sequence\nUniform distribution")
 iArray = np.arange(0, maxPoints, 1)  # array of integers
 xArray = np.random.random_sample(maxPoints)  # array of random numbers
-plt.plot(iArray, xArray, color="blue", label="array", linestyle="None", marker="x")
 
-# other stuff for plot
-plt.xlabel("i")
+plt.plot(iArray, xArray, color="blue", label="array", linestyle="None", marker=".")
+
+plt.ylabel("i")
 plt.xlabel("Number")
-plt.legend()
+plt.legend(loc="upper right", framealpha=0.96)
 plt.show()
-
-# Playing around -- delete for lab
-"""
-second_plot = gdisplay(xtitle='x_i+1', ytitle='x_i', width=500, height=500)
-crazy_correlations = gdots(color=color.green)
-for i in range(maxPoints-1):
-    rate(10000)
-    #print(xArray[i], xArray[i+1])
-    crazy_correlations.plot(pos=(xArray[i], xArray[i+1]))
-
-print(np.average(xArray))
-print(np.var(xArray))
-"""
