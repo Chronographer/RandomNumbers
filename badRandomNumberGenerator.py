e9de360b2e0cf3b6d60621dfd6ec9346c424e3ee
Daniel <djisenberg2015@gmail.com>
2020-06-23 16:37:58 -0400

    added second plot function to plot ri vs i instead of correlation. Moved x and y axis lists into the functions that use them to reduce scope. refactored comparison list to better match its counter part.

--- badRandomNumberGenerator.py
+++ badRandomNumberGenerator.py
@@ -1,5 +1,4 @@
 import matplotlib.pyplot as plt
-import color
 
 a = 57
 c = 1
@@ -7,8 +6,6 @@ m = 256
 seed = 10
 
 maxNumber = 1000
-xAxisList = []
-yAxisList = []
 
 
 def generateRandomNumberLinearCongruence():
@@ -22,6 +19,8 @@ def generateRandomNumberLinearCongruence():
 
 
 def plotRandomNumberCorrelation(randomNumberList):
+    xAxisList = []
+    yAxisList = []
     for index in range(1, len(randomNumberList)):
         xAxisList.append(randomNumberList[index-1])
         yAxisList.append(randomNumberList[index])
@@ -33,60 +32,78 @@ def plotRandomNumberCorrelation(randomNumberList):
     plt.show()
 
 
+def plotRandomNumberInOrder(randomNumberList):
+    xAxisList = []
+    yAxisList = []
+    for index in range(0, len(randomNumberList)):
+        xAxisList.append(index)
+        yAxisList.append(randomNumberList[index])
+    plt.plot(xAxisList, yAxisList, 'b.')
+    plt.suptitle("'Bad' Random number generator")
+    plt.xlabel("i")
+    plt.ylabel("ri")
+    plt.grid(True)
+    plt.show()
+
+
 def computePatternLength(randomNumberList):
     print("\nNOTE: This program does not account for the possibility that the first instance of a repeating pattern is not itself a part of a larger pattern.\nTry re-running 'computePatternLength()' using 'manualList' as an argument for details.\n")
-    comparisonList = []
+    firstHalfOfTheList = []
     secondHalfOfTheList = []
     for index in range(0, len(randomNumberList)):
-        print("\nLooking for repetition in the first " + str(index) + " indices...")
+        # print("\nLooking for repetition in the first " + str(index) + " indices...")
         for i in range(0, index):
             if i < int(index/2):
-                comparisonList.append(randomNumberList[i])
+                firstHalfOfTheList.append(randomNumberList[i])
             elif i >= int(index/2):
                 secondHalfOfTheList.append(randomNumberList[i])
 
-        if comparisonList == secondHalfOfTheList:
-            if len(comparisonList) > 0:
-                print("match found! Pattern repeats every " + str(len(comparisonList)) + " values!")
-                print("comparisonList: " + str(comparisonList))
+        if firstHalfOfTheList == secondHalfOfTheList:
+            if len(firstHalfOfTheList) > 0:
+                print('\033[1m' + "match found!" + '\033[0m' + " Pattern repeats every " + str(len(firstHalfOfTheList)) + " values!")
+                print("firstHalfOfTheList: " + str(firstHalfOfTheList))
                 print("secondHalfOfTheList: " + str(secondHalfOfTheList))
                 return
-        comparisonList.clear()
+        firstHalfOfTheList.clear()
         secondHalfOfTheList.clear()
 
+
 def computePatternLengthWarnSubPatterns(randomNumberList):
-    comparisonList = []
+    FirstHalfOfTheList = []
     secondHalfOfTheList = []
     repeatLength = []
-    equilizedRepeatLength = []
+    equalizedRepeatLength = []
     for index in range(0, len(randomNumberList)):
-        #print("\nLooking for repetition in the first " + str(index) + " indices...")
         for i in range(0, index):
             if i < int(index/2):
-                comparisonList.append(randomNumberList[i])
+                FirstHalfOfTheList.append(randomNumberList[i])
             elif i >= int(index/2):
                 secondHalfOfTheList.append(randomNumberList[i])
 
-        if comparisonList == secondHalfOfTheList:
-            if len(comparisonList) > 0:
-                print("match found! Pattern repeats every " + str(len(comparisonList)) + " values!")
-                print("comparisonList: " + str(comparisonList))
+        if FirstHalfOfTheList == secondHalfOfTheList:
+            if len(FirstHalfOfTheList) > 0:
+                print("match found! Pattern repeats every " + str(len(FirstHalfOfTheList)) + " values!")
+                print("FirstHalfOfTheList: " + str(FirstHalfOfTheList))
                 print("secondHalfOfTheList: " + str(secondHalfOfTheList))
-                repeatLength.append(len(comparisonList))
-                #return
-        comparisonList.clear()
+                repeatLength.append(len(FirstHalfOfTheList))
+        FirstHalfOfTheList.clear()
         secondHalfOfTheList.clear()
     for i in range(0, len(repeatLength)):
         if i == 0:
-            equilizedRepeatLength.append(repeatLength[i])
+            equalizedRepeatLength.append(repeatLength[i])
         else:
-            equilizedRepeatLength.append(repeatLength[i] - repeatLength[i-1])
-    for i in range(0, len(equilizedRepeatLength)):
-        if equilizedRepeatLength[i] != equilizedRepeatLength[i-1]:
+            equalizedRepeatLength.append(repeatLength[i] - repeatLength[i-1])
+    for i in range(0, len(equalizedRepeatLength)):
+        if equalizedRepeatLength[i] != equalizedRepeatLength[i-1]:
             print('\n' + '\033[1m' + '\033[91m' + "WARNING:" + '\033[0m' + " initial repeating pattern is itself a part of a larger pattern!")
             break
 
 
-manualList = [1, 2, 3, 1, 2, 3, 5, 1, 2, 3, 1, 2, 3, 5, 1, 2, 3, 1, 2, 3, 5, 1, 2, 3, 1, 2, 3, 5, 1, 2, 3, 1, 2, 3, 5, 1, 2, 3, 1, 2, 3, 5]  # Use this to demonstrate the limitations of 'computePatternLength()'
-#computePatternLengthWarnSubPatterns(manualList)
-computePatternLength(generateRandomNumberLinearCongruence())
+'''
+manualList = [1, 2, 3, 1, 2, 3, 5, 1, 2, 3, 1, 2, 3, 5, 1, 2, 3, 1, 2, 3, 5, 1, 2, 3, 1, 2, 3, 5, 1, 2, 3, 1, 2, 3, 5, 1, 2, 3, 1, 2, 3, 5]
+computePatternLengthWarnSubPatterns(manualList) # Use this to demonstrate the limitations of 'computePatternLength()
+'''
+randomList = generateRandomNumberLinearCongruence()
+computePatternLength(randomList)
+plotRandomNumberCorrelation(randomList)
+plotRandomNumberInOrder(randomList)
