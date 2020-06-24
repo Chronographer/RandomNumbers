99b0b223697e33876048e6d219a0fa68a739c5a0
Daniel <djisenberg2015@gmail.com>
2020-06-24 13:29:00 -0400

    removed unneeded function computePatternLengthWarnSubPatterns(), tweaked print statements to possibly make it a bit cleaner.

--- badRandomNumberGenerator.py
+++ badRandomNumberGenerator.py
@@ -17,7 +17,7 @@ def generateRandomNumberLinearCongruence():
         currentRandomNumber = (currentRandomNumber * a + c) % m
         randomNumberList.append(currentRandomNumber)
         #print(currentRandomNumber)
-    print("done.")
+    #print("done.")
     return randomNumberList
 
 
@@ -26,7 +26,7 @@ def generateUniformRandomNumber(randomNumberList):
     uniformList = []
     for i in range(0, len(randomNumberList)):
         uniformList.append(randomNumberList[i] / m)
-    print("done.")
+    #print("done.")
     return uniformList
 
 
@@ -36,7 +36,7 @@ def generateRandomNumberNumPY():
     for i in range(0, maxNumber):
         x = random.random_sample()
         randomNumberList.append(x)
-    print("done.")
+    #print("done.")
     return randomNumberList
 
 
@@ -53,7 +53,7 @@ def plotRandomNumberCorrelation(randomNumberList):
     plt.ylabel("ri")
     plt.grid(True)
     plt.show()
-    print("done.")
+    #print("done.")
 
 
 def plotRandomNumberInOrder(randomNumberList):
@@ -69,7 +69,7 @@ def plotRandomNumberInOrder(randomNumberList):
     plt.ylabel("ri")
     plt.grid(True)
     plt.show()
-    print("done.")
+    #print("done.")
 
 
 def computePatternLength(randomNumberList):
@@ -96,54 +96,18 @@ def computePatternLength(randomNumberList):
         secondHalfOfTheList.clear()
     if patternRepeatFlag is False:
         print("No repeating pattern found.")
-    print("done.")
-
-
-def computePatternLengthWarnSubPatterns(randomNumberList):
-    FirstHalfOfTheList = []
-    secondHalfOfTheList = []
-    repeatLength = []
-    equalizedRepeatLength = []
-    for index in range(0, len(randomNumberList)):
-        for i in range(0, index):
-            if i < int(index/2):
-                FirstHalfOfTheList.append(randomNumberList[i])
-            elif i >= int(index/2):
-                secondHalfOfTheList.append(randomNumberList[i])
-
-        if FirstHalfOfTheList == secondHalfOfTheList:
-            if len(FirstHalfOfTheList) > 0:
-                print("match found! Pattern repeats every " + str(len(FirstHalfOfTheList)) + " values!")
-                print("FirstHalfOfTheList: " + str(FirstHalfOfTheList))
-                print("secondHalfOfTheList: " + str(secondHalfOfTheList))
-                repeatLength.append(len(FirstHalfOfTheList))
-        FirstHalfOfTheList.clear()
-        secondHalfOfTheList.clear()
-    for i in range(0, len(repeatLength)):
-        if i == 0:
-            equalizedRepeatLength.append(repeatLength[i])
-        else:
-            equalizedRepeatLength.append(repeatLength[i] - repeatLength[i-1])
-    for i in range(0, len(equalizedRepeatLength)):
-        if equalizedRepeatLength[i] != equalizedRepeatLength[i-1]:
-            print('\n' + '\033[1m' + '\033[91m' + "WARNING:" + '\033[0m' + " initial repeating pattern is itself a part of a larger pattern!")
-            break
+    #print("done.")
 
 
 def computeUniformity(randomNumberList, k):
-    print("computing uniformity of distribution for k = " + str(k) + "...")
+    print("computing uniformity of distribution for k=" + str(k) + "...")
     sequenceLength = len(randomNumberList)
     sumX = 0
     for i in range(0, sequenceLength):
         sumX = sumX + randomNumberList[i] ** k
     xK = (1 / sequenceLength) * sumX
-    print("done.\nkth moment of distribution where k is " + str(k) + " is: " + str(xK))
+    print("moment of distribution where k=" + str(k) + " is: " + str(xK))
 
-
-'''
-manualList = [1, 2, 3, 1, 2, 3, 5, 1, 2, 3, 1, 2, 3, 5, 1, 2, 3, 1, 2, 3, 5, 1, 2, 3, 1, 2, 3, 5, 1, 2, 3, 1, 2, 3, 5, 1, 2, 3, 1, 2, 3, 5]
-computePatternLengthWarnSubPatterns(manualList) # Use this to demonstrate the limitations of 'computePatternLength()
-'''
 
 uniformRandomList = generateUniformRandomNumber(generateRandomNumberLinearCongruence())
 #uniformRandomList = generateRandomNumberNumPY()
