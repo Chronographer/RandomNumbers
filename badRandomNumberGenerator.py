071027b74fa29658a840df74ef27f69cdd85e768
Daniel <djisenberg2015@gmail.com>
2020-06-24 13:21:02 -0400

    Made plot titles more descriptive, removed note about possible flaw in computePatternLength() because it relates to a problem that does not generate naturally, added function to compute the uniformity x^k of a set of random numbers, added function generateUniformRandomNumber() which converts a list of random numbers given to it into a list of uniform random numbers between 0 and 1. Added various print statements to provide the status of the program, particularly when it is being run for a very large number of random numbers. Changed computePatternLength() to now inform the user if no pattern is identified.

--- badRandomNumberGenerator.py
+++ badRandomNumberGenerator.py
@@ -1,57 +1,84 @@
 import matplotlib.pyplot as plt
+import numpy.random as random
 
-a = 57
-c = 1
-m = 256
+a = 25214903917
+c = 11
+m = 2 ** 48
 seed = 10
 
 maxNumber = 1000
 
 
 def generateRandomNumberLinearCongruence():
+    print("Generating list of random numbers using linear congruence method...")
     currentRandomNumber = seed
     randomNumberList = [seed]
     for index in range(0, maxNumber):
         currentRandomNumber = (currentRandomNumber * a + c) % m
         randomNumberList.append(currentRandomNumber)
-        # print(currentRandomNumber)
+        #print(currentRandomNumber)
+    print("done.")
+    return randomNumberList
+
+
+def generateUniformRandomNumber(randomNumberList):
+    print("scaling list of random numbers between 0 and 1...")
+    uniformList = []
+    for i in range(0, len(randomNumberList)):
+        uniformList.append(randomNumberList[i] / m)
+    print("done.")
+    return uniformList
+
+
+def generateRandomNumberNumPY():
+    print("Generating list of random numbers using numPy...")
+    randomNumberList = []
+    for i in range(0, maxNumber):
+        x = random.random_sample()
+        randomNumberList.append(x)
+    print("done.")
     return randomNumberList
 
 
 def plotRandomNumberCorrelation(randomNumberList):
+    print("plotting correlation between subsequently generated random numbers...")
     xAxisList = []
     yAxisList = []
     for index in range(1, len(randomNumberList)):
         xAxisList.append(randomNumberList[index-1])
         yAxisList.append(randomNumberList[index])
     plt.plot(xAxisList, yAxisList, 'b.')
-    plt.suptitle("'Bad' Random number generator")
+    plt.suptitle("Correlation between element i and i-1\nusing 'good' linear congruence method")
     plt.xlabel("ri-1")
     plt.ylabel("ri")
     plt.grid(True)
     plt.show()
+    print("done.")
 
 
 def plotRandomNumberInOrder(randomNumberList):
+    print("plotting random numbers in the order they were generated...")
     xAxisList = []
     yAxisList = []
     for index in range(0, len(randomNumberList)):
         xAxisList.append(index)
         yAxisList.append(randomNumberList[index])
     plt.plot(xAxisList, yAxisList, 'b.')
-    plt.suptitle("'Bad' Random number generator")
+    plt.suptitle("Random numbers generated using 'good'\nlinear congruence method in order of their generation")
     plt.xlabel("i")
     plt.ylabel("ri")
     plt.grid(True)
     plt.show()
+    print("done.")
 
 
 def computePatternLength(randomNumberList):
-    print("\nNOTE: This program does not account for the possibility that the first instance of a repeating pattern is not itself a part of a larger pattern.\nTry re-running 'computePatternLength()' using 'manualList' as an argument for details.\n")
+    print("attempting to identify a repeating pattern...")
     firstHalfOfTheList = []
     secondHalfOfTheList = []
+    patternRepeatFlag = False
     for index in range(0, len(randomNumberList)):
-        # print("\nLooking for repetition in the first " + str(index) + " indices...")
+        #print("\nLooking for repetition in the first " + str(index) + " indices...")
         for i in range(0, index):
             if i < int(index/2):
                 firstHalfOfTheList.append(randomNumberList[i])
@@ -63,9 +90,13 @@ def computePatternLength(randomNumberList):
                 print('\033[1m' + "match found!" + '\033[0m' + " Pattern repeats every " + str(len(firstHalfOfTheList)) + " values!")
                 print("firstHalfOfTheList: " + str(firstHalfOfTheList))
                 print("secondHalfOfTheList: " + str(secondHalfOfTheList))
+                patternRepeatFlag = True  # let the program know that the pattern does repeat
                 return
         firstHalfOfTheList.clear()
         secondHalfOfTheList.clear()
+    if patternRepeatFlag is False:
+        print("No repeating pattern found.")
+    print("done.")
 
 
 def computePatternLengthWarnSubPatterns(randomNumberList):
@@ -99,11 +130,26 @@ def computePatternLengthWarnSubPatterns(randomNumberList):
             break
 
 
+def computeUniformity(randomNumberList, k):
+    print("computing uniformity of distribution for k = " + str(k) + "...")
+    sequenceLength = len(randomNumberList)
+    sumX = 0
+    for i in range(0, sequenceLength):
+        sumX = sumX + randomNumberList[i] ** k
+    xK = (1 / sequenceLength) * sumX
+    print("done.\nkth moment of distribution where k is " + str(k) + " is: " + str(xK))
+
+
 '''
 manualList = [1, 2, 3, 1, 2, 3, 5, 1, 2, 3, 1, 2, 3, 5, 1, 2, 3, 1, 2, 3, 5, 1, 2, 3, 1, 2, 3, 5, 1, 2, 3, 1, 2, 3, 5, 1, 2, 3, 1, 2, 3, 5]
 computePatternLengthWarnSubPatterns(manualList) # Use this to demonstrate the limitations of 'computePatternLength()
 '''
-randomList = generateRandomNumberLinearCongruence()
-computePatternLength(randomList)
-plotRandomNumberCorrelation(randomList)
-plotRandomNumberInOrder(randomList)
+
+uniformRandomList = generateUniformRandomNumber(generateRandomNumberLinearCongruence())
+#uniformRandomList = generateRandomNumberNumPY()
+computePatternLength(uniformRandomList)
+plotRandomNumberCorrelation(uniformRandomList)
+plotRandomNumberInOrder(uniformRandomList)
+computeUniformity(uniformRandomList, 1)
+computeUniformity(uniformRandomList, 2)
+
